e6be7470ddf1698a97384cda66cfd970fd3af8c8
Zzzzzshan <zshanfarooq67@gmail.com>
2023-01-12 23:57:44 +0400

    tasks

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,8 +1,35 @@
-import { useState } from "react";
+// import { useState } from "react";
+
+const Tasks = [
+  {
+    id: 1,
+    text: "Docters Appointment",
+    day: "Feb 5th at 2:30pm",
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: "Meeting at School",
+    day: "Feb 6th at 1:30pm",
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: "Food Shopping",
+    day: "Feb 5th at 2:30pm",
+    reminder: false,
+  },
+];
 
 const Tasks = () => {
-  const [tasks, setTasks] = useState();
-  return <>{Tasks.map}</>;
+  // const [tasks, setTasks] = useState();
+  return (
+    <>
+      {Tasks.map((tasks) => (
+        <h3>{tasks.text}</h3>
+      ))}
+    </>
+  );
 };
 
 export default Tasks;
