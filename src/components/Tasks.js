60af438484f70cf23d4ddeb9c962b2a4f9dcc308
Zzzzzshan <zshanfarooq67@gmail.com>
2023-01-13 00:48:44 +0400

    Toggle Delete implemented

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,32 +1,15 @@
-// import { useState } from "react";
+import Task from "./Task";
 
-const Tasks = [
-  {
-    id: 1,
-    text: "Docters Appointment",
-    day: "Feb 5th at 2:30pm",
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: "Meeting at School",
-    day: "Feb 6th at 1:30pm",
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: "Food Shopping",
-    day: "Feb 5th at 2:30pm",
-    reminder: false,
-  },
-];
-
-const Tasks = () => {
-  // const [tasks, setTasks] = useState();
+const Tasks = ({ tasks, onDelete, onToggle }) => {
   return (
     <>
-      {Tasks.map((tasks) => (
-        <h3>{tasks.text}</h3>
+      {tasks.map((task) => (
+        <Task
+          key={task.id}
+          task={task}
+          onDelete={onDelete}
+          onToggle={onToggle}
+        />
       ))}
     </>
   );
