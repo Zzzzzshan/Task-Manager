1396be45dae0e8bfde6524a24fa5040e957647b4
Zzzzzshan <zshanfarooq67@gmail.com>
2023-01-15 00:42:55 +0400

    Finally Done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
+
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([]);
@@ -16,6 +18,13 @@ const App = () => {
   //fetched tasks
   const fetchTasks = async () => {
     const res = await fetch("http://localhost:5000/tasks");
+    const data = await res.json();
+    return data;
+  };
+
+  //fetched task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
     const data = await res.json();
     return data;
   };
@@ -46,10 +55,23 @@ const App = () => {
     setTasks(tasks.filter((task) => task.id !== id));
   };
   //Toggle Reminder:
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updTask),
+    });
+
+    const data = await res.json();
+
     setTasks(
       tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       )
     );
   };
@@ -66,6 +88,7 @@ const App = () => {
       ) : (
         "No Tasks To Show"
       )}
+      <Footer />
     </div>
   );
 };
